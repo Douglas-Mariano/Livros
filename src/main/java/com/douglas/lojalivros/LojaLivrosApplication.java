bc70ded9ec73d646d8ca75bd81a1eebaa35d3265
douglasmariano <douglas.arm@live.com>
2023-04-19 15:18:04 -0300

    Primeiro commit criação do projeto

---
+++ src/main/java/com/douglas/lojalivros/LojaLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.lojalivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglas/lojalivros/LojaLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.lojalivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
