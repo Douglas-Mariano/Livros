5c84afafe53ca58a04e3bb07f2d4c0257167245d
douglasmariano <douglas.arm@live.com>
2023-04-19 16:29:15 -0300

    Desenvolvendo as Entidades

---
+++ src/main/java/com/douglas/lojalivros/entity/Autor.java
@@ -0,0 +1,25 @@
+package com.douglas.lojalivros.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer ano;
+}
---
+++ src/main/java/com/douglas/lojalivros/entity/Livros.java
@@ -0,0 +1,38 @@
+package com.douglas.lojalivros.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livros {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer paginas;
+
+    @Column(nullable = false)
+    private Integer capitulos;
+
+    @Column(nullable = false)
+    private String identificacao;
+
+    @Column(name = "nome_editora", nullable = false, unique = true)
+    private String editora;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+}
